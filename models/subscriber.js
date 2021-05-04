5700e737530ea8663068745743e1cd0fd7528180
akashvaghela09 <akashvaghela09@gmail.com>
2021-05-04 15:29:09 +0530

    model and route changed

---
+++ models/subscriber.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const subscriberSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('subscriber', subscriberSchema)
